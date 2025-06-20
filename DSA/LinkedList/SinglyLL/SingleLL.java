10eda597760f1049b64f102d60ab73101282c74b
KabirSingh-1 <singhkabir44778@gmail.com>
2025-06-20 16:55:48 +0530

    Update SingleLL.java

--- DSA/LinkedList/SinglyLL/SingleLL.java
+++ DSA/LinkedList/SinglyLL/SingleLL.java
@@ -21,6 +21,7 @@ public class SingleLL {
         System.out.println("null");
     }
 
+ // add at first
     public  void addFirst(int data){
       Node NewNode = new Node(0);
 
@@ -32,7 +33,9 @@ public class SingleLL {
       NewNode.next = Head;
       Head = NewNode;
     }
-    
+
+
+ // add at last
     public void addLast(int data){
         Node NewNode = new Node(data);
         if(Head == null){
@@ -47,13 +50,36 @@ public class SingleLL {
         temp.next = NewNode;
     }
 
+ // add at a given position 
+    public void addAtGivenPosition(int data, int position){
+        Node NewNode = new Node(data);
+        if(position == 1){
+          
+            NewNode.next = Head;
+            Head = NewNode;
+            return;
+        }
+
+        Node temp = Head;
+        int i = 1;
+        while(temp!=null && i<position-1){
+           temp = temp.next;
+           i++;
+        }
+        if(temp == null) return;
+        NewNode.next = temp.next;
+        temp.next = NewNode;
+    }
+
+ // delete first
+
     public void deleteFirst(){
         if(Head == null){
             return;
         }
         Head = Head.next;
     }
-
+ // delete end
     public void deleteEnd(){
         if(Head == null) return;
         Node temp = Head;
@@ -61,6 +87,23 @@ public class SingleLL {
             temp = temp.next;
         }
         temp.next = null;
+    }
+
+ // delete a given key 
+     public void deleteGivenKey(int key){
+        if(Head == null && Head.next == null) return ;
+        if(Head.data == key){
+            Head = Head.next;
+            return;
+        }
+        Node temp = Head;
+        while(temp.next.data!=key){
+            temp = temp.next;
+        }
+          if (temp.next != null) {
+        temp.next = temp.next.next;
+    }
+
     }
 
     public static void main(String[] args) {
@@ -76,5 +119,10 @@ public class SingleLL {
        list.printLL(list.Head);
        list.deleteEnd();
        list.printLL(list.Head);
+       list.addAtGivenPosition(8, 3);
+       list.printLL(list.Head);
+       list.deleteGivenKey(8);
+       list.printLL(list.Head);
+     
     }
 }
