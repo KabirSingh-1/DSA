c660a24c69fd6067826b0ac505a6feb41c24d02c
Kabir Singh <kabirsingh44778@gmail.com>
2025-07-04 16:16:24 +0530

    sum of nodes function

--- Tree/CountNode.java
+++ Tree/CountNode.java
@@ -31,12 +31,22 @@ public class CountNode {
        int rightCount = Count(root.right);
        return leftCount+rightCount+1;
     }
+
+    public static int sumOfNodes(Node root){
+        if(root == null){
+            return 0;
+        }
+        int leftSum = sumOfNodes(root.left);
+        int rightSum = sumOfNodes(root.right);
+        return leftSum+rightSum+root.data;
+    }
     public static void main(String[] args) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
 
         System.out.println("Total Nodes: " + Count(root));
+        System.out.println("Total Sum Of Nodes: "+sumOfNodes(root));
 
     }
 }
