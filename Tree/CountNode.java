4fb260d5db4cb279cbdba8948baa536307764c90
Kabir Singh <kabirsingh44778@gmail.com>
2025-07-04 16:26:11 +0530

    height function

--- Tree/CountNode.java
+++ Tree/CountNode.java
@@ -40,6 +40,14 @@ public class CountNode {
         int rightSum = sumOfNodes(root.right);
         return leftSum+rightSum+root.data;
     }
+
+    public static int height(Node root){
+        if(root == null)return 0;
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        int MyHeight = Math.max(leftHeight, rightHeight)+1;
+        return MyHeight;
+    }
     public static void main(String[] args) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree = new BinaryTree();
@@ -47,6 +55,7 @@ public class CountNode {
 
         System.out.println("Total Nodes: " + Count(root));
         System.out.println("Total Sum Of Nodes: "+sumOfNodes(root));
+        System.out.println("height of binary tree: "+height(root));
 
     }
 }
