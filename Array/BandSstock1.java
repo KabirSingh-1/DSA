c9a1baf2577f791206ed7bd3258bf0c77f673e9d
Kabir Singh <kabirsingh44778@gmail.com>
2025-07-07 14:10:08 +0530

    update in BandSstock1

--- Array/BandSstock1.java
+++ Array/BandSstock1.java
@@ -1,7 +1,11 @@
 
 public class BandSstock1 {
     public static void main(String[] args) {
-        int[] prices = {7,6,4,3,1};
+      int[] prices = {7,6,4,3,1};
+      if (prices == null || prices.length < 2){
+        System.out.println(0);
+        return;
+      }
         int buy= prices[0];
         int profit = 0;
         for(int i = 1; i<prices.length;i++){
