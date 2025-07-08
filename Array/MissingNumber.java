93f66f1fba92ce1b65eeb8a174e345a8a56138e4
Kabir Singh <kabirsingh44778@gmail.com>
2025-07-08 16:57:26 +0530

    solve missing number

---
+++ Array/MissingNumber.java
@@ -0,0 +1,17 @@
+//Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+public class MissingNumber {
+    public static int Missing(int nums[]){
+        int n = nums.length;
+        int total = (n)*(n+1)/2;
+        int sum = 0;
+        for(int num : nums){
+            sum+=num;
+        }
+        return total-sum;
+    }
+    public static void main(String[] args) {
+        int nums[] = {3,0,1};
+        System.out.println(Missing(nums));
+    }
+}
