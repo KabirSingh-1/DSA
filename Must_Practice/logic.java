9d3db98d636d02f2894120fbce838a40a95018c4
Kabir Singh <kabirsingh44778@gmail.com>
2025-07-14 11:23:26 +0530

    add primNumber

---
+++ Must_Practice/logic.java
@@ -0,0 +1,15 @@
+package Must_Practice;
+public class logic {
+        public static boolean prim(int num){
+        boolean prime  = true;
+            if(num <=1) return false;
+            for(int j = 2; j<=Math.sqrt(num); j++){
+                if(num%j==0)return false;
+                break;
+        
+            
+        }
+       return prime;
+    }
+    
+}
---
+++ Must_Practice/primeNumber.java
@@ -0,0 +1,20 @@
+package Must_Practice;
+
+import java.util.Scanner;
+
+public class primeNumber extends logic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int nums[] = new int[10];
+        
+        for(int i = 0; i<nums.length; i++){
+             nums[i] = sc.nextInt();
+             if(prim(nums[i])){
+                 System.out.println("prime");
+             }
+             else{
+                System.out.println("non prime");
+             }
+        }
+    }  
+}
