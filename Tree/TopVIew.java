618656935493c469c84d0759c293142ec198f0d4
Kabir Singh <kabirsingh44778@gmail.com>
2025-07-25 17:08:10 +0530

    add

--- Tree/TopVIew.java
+++ Tree/TopVIew.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TopVIew {
    static class Node {
       int data;
@@ -7,6 +12,14 @@ public class TopVIew {
          this.data = data;
          this.left = null;
          this.right = null;
+      }
+   }
+   static class pair{
+      Node node;
+      int count;
+      pair(Node node,int count){
+         this.node = node;
+         this.count = count;
       }
    }
     static class BinaryTree{
@@ -23,7 +36,37 @@ public class TopVIew {
         }
 
     }
+public static ArrayList<Integer> level(Node root){
+   ArrayList<Integer> list = new ArrayList<>();
+   if(root == null){ return list;}
+   Queue<pair> queue = new LinkedList<>();
+   queue.add(new pair(root,0));
+   HashMap<Integer ,Integer> map = new HashMap<>();
+   while(!queue.isEmpty()){       
+         pair curr = queue.poll();
+         Node node = curr.node;
+         int hd = curr.count;
+         if(!map.containsKey(hd)){
+            map.put(hd,node.data);
+         }
+         if(node.left!=null){
+            queue.add(new pair(node.left, hd-1));
+         }
+         if(node.right!=null){
+            queue.add(new pair(node.right, hd+1));
+         }
+      }
+      for(int nodes : map.values()){
+         list.add(nodes);  
+      }
+   return list;
+}
    public static void main(String[] args) {
      int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+     BinaryTree tree = new BinaryTree();
+     Node root = tree.buildTree(nodes);
+     System.out.println("root: "+root.data);
+     System.out.println("level order traversal");
+     System.out.println(level(root));
    } 
 }
