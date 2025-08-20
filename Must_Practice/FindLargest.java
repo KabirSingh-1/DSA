43db50f3e32a0dc7de8d658e49a7848278e407fa
KabirSingh-1 <singhkabir44778@gmail.com>
2025-08-20 18:51:56 +0530

    changes

--- Must_Practice/FindLargest.java
+++ Must_Practice/FindLargest.java
@@ -16,6 +16,9 @@ public class FindLargest {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int nums[] = new int[n];
+        for(int i = 0; i<n; i++){
+            nums[i] = sc.nextInt();
+        }
         System.out.println(Largest(nums));
         sc.close();
     }
