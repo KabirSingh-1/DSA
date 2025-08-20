5ba945e1d647201b080e49170165919d0cde151e
KabirSingh-1 <singhkabir44778@gmail.com>
2025-08-20 18:53:06 +0530

    changes

--- Must_Practice/FindSmallest.java
+++ Must_Practice/FindSmallest.java
@@ -1,5 +1,7 @@
 package Must_Practice;
 
+import java.util.Scanner;
+
 public class FindSmallest {
     public static int Smallest(int nums[]){
         int min = Integer.MAX_VALUE;
@@ -11,7 +13,14 @@ public class FindSmallest {
         return min;
     }
     public static void main(String[] args) {
-        int nums[] = {44,50,78,29,87,45,33};
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int nums[] = new int[n];
+        for(int i = 0; i<n; i++){
+            nums[i] = sc.nextInt();
+        }
+
         System.out.println(Smallest(nums));
+        sc.close();
     }
 }
