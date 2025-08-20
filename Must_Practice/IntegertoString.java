cb2ec43792dd175a491d8900277179ad540a2f01
KabirSingh-1 <singhkabir44778@gmail.com>
2025-08-20 18:54:09 +0530

    changes

--- Must_Practice/IntegertoString.java
+++ Must_Practice/IntegertoString.java
@@ -4,14 +4,11 @@ import java.util.Scanner;
 
 public class IntegertoString {
     public static void main(String[] args) {
-        // string to int
-
- 
+        //change string to int
         String str = "1234";
         int num = Integer.parseInt(str);
         System.out.println(num);
-
-        // int to string
+        //change  int to string
         int num2 = 1234;
         String str1 = Integer.toString(num2);
         System.out.println(str1);
