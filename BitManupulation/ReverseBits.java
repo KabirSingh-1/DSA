b1e3b255cd024a2ace0b03c271706611389a4369
KabirSingh-1 <singhkabir44778@gmail.com>
2025-08-27 19:39:02 +0530

    solve reverse bits

---
+++ BitManupulation/ReverseBits.java
@@ -0,0 +1,33 @@
+package BitManupulation;
+
+import java.util.Scanner;
+
+public class ReverseBits {
+    public static int approach1(int n){
+       int result = 0;
+       int helper = 1;
+       for(int i = 0; i<32; i++){
+        if((helper&n)!=0){
+            result+=1<<31-i;
+        }
+        helper<<=1;
+       }
+       return result;
+    }
+    public static int approach2(int n){
+       int result = 0;
+       for(int i = 0; i<32; i++){
+        result<<=1;
+        result|=(n&1);
+        n>>=1;
+       }
+       return result;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(approach1(n));
+        System.out.println(approach2(n));
+        sc.close();
+    }
+}
