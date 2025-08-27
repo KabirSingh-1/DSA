339f53506fe882ad2b25ba8b8628a725b5d5807f
KabirSingh-1 <singhkabir44778@gmail.com>
2025-08-27 16:49:49 +0530

    approach 1

--- BitManupulation/numberOf1Bit.java
+++ BitManupulation/numberOf1Bit.java
@@ -1,7 +1,24 @@
 package BitManupulation;
 
+import java.util.Scanner;
+
 public class numberOf1Bit {
+    public static int approach1(int n){
+        int count = 0;
+        String str = Integer.toBinaryString(n);
+        char ch[] = str.toCharArray();
+        for(char c : ch){
+            if(c == '1'){
+                count++;
+            }
+        }
+        return count;
+    }
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter the number: ");
+        int n = sc.nextInt();
+        System.out.println("approach one : "+approach1(n));
+        sc.close();
     }
 }
