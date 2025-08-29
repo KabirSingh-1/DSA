ce8ccc65c72795921a6e94e3f87d39a450a8a1c9
KabirSingh-1 <singhkabir44778@gmail.com>
2025-08-29 21:12:49 +0530

    change

--- BitManupulation/SumofTwoIntegers.java
+++ BitManupulation/SumofTwoIntegers.java
@@ -16,9 +16,11 @@ public class SumofTwoIntegers {
     }
     public static void main(String[] args) {
         Scanner sc =  new Scanner(System.in);
+        System.out.print("enter first integer: ");
         int a = sc.nextInt();
+        System.out.print("enter second integer: ");
         int b = sc.nextInt();
-        System.out.println("sum of two integers without using +: "+sumofTwoInteger(a, b));
+        System.out.println("sum of two integers without using + : "+sumofTwoInteger(a, b));
 
         sc.close();
 
